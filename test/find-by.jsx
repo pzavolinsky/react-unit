3e30f31b22ff30991f89f1bc23ac3541546f19c7
Patricio Zavolinsky <patricio.zavolinsky@emaint.com>
2015-11-12 16:04:48 +0000

    Moved findByRef tests to a separate file.

--- test/find-by.jsx
+++ test/find-by.jsx
@@ -4,11 +4,11 @@ var React = require('react');
 
 var Bouviers = React.createClass({
   render: function() {
-    return <div name="Jacqueline Bouvier" ref="bouvier">
-      <div name="Marge Bouvier" ref="bouvier">
-        <div name="Lisa Simpson" ref="simpson" />
+    return <div name="Jacqueline Bouvier">
+      <div name="Marge Bouvier">
+        <div name="Lisa Simpson" />
       </div>
-      <input name="Patty Bouvier" ref="bouvier" />
+      <input name="Patty Bouvier" />
     </div>
   }
 });
@@ -38,25 +38,5 @@ describe('findBy', () => {
     expect(bouviers[0].props.name).toEqual('Jacqueline Bouvier');
     expect(bouviers[1].props.name).toEqual('Marge Bouvier');
     expect(bouviers[2].props.name).toEqual('Patty Bouvier');
-  });
-});
-
-describe('findByRef', () => {
-  it('should call the findBy method', () => {
-    var component = createComponent(<Bouviers/>);
-    spyOn(component, 'findBy');
-    component.findByRef('myRef');
-
-    expect(component.findBy).toHaveBeenCalled();
-  });
-
-  it('should find by ref attribute', () => {
-    var component = createComponent(<Bouviers/>);
-    var bouviers = component.findByRef('bouvier');
-    var simpsons = component.findByRef('simpson');
-
-    expect(bouviers.length).toEqual(3);
-    expect(simpsons.length).toEqual(1);
-    expect(simpsons[0].props.name).toEqual('Lisa Simpson');
   });
 });
